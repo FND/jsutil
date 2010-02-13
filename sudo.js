92c756caccf7fdeea321a311c4242f9bce6c65fe
FND <fnd@oda.(none)>
2010-02-13 11:24:45 +0000

    sudo: clarified documentation
    also fixed documentation formatting
    plus renamed misnamed argument

--- sudo.js
+++ sudo.js
@@ -1,15 +1,11 @@
-/*
- * execute function with enhanced privileges (on Mozilla-based browsers)
- *
- * This is primarily useful for XHRs from a local file.
- *
- * inspired by TiddlyWiki (http://tiddlywiki.com)
- */
-var sudo = function(callback) {
+// executes function with enhanced privileges (on Mozilla-based browsers)
+// This is primarily useful when performing XHRs from a file:// URI.
+// inspired by TiddlyWiki (http://tiddlywiki.com)
+var sudo = function(fn) {
 	if(document.location.protocol.indexOf("file") == 0 && window.Components &&
 		window.netscape && window.netscape.security) {
 		window.netscape.security.PrivilegeManager.
 			enablePrivilege("UniversalBrowserRead");
 	}
-	return callback();
+	return fn();
 };
